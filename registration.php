a44a93a2b585ef0c8a386472168dd9f9e6402893
HasanHero1 <63868459+HasanHero1@users.noreply.github.com>
2020-04-22 02:44:35 +0600

    Update registration.php

--- registration.php
+++ registration.php
@@ -55,8 +55,8 @@
 				$mail -> isSMTP();
 				$mail -> Host       = "smtp.gmail.com";
 				$mail -> SMTPAuth   = "true";
-				$mail -> Username   = "itshasan573@gmail.com";
-				$mail -> Password   = "DJ...SAGOR  2468";
+				$mail -> Username   = "***********";
+				$mail -> Password   = "***********";
 				$mail -> SMTPSecure = "tls";
 				$mail -> Port       = 587;
 				$mail -> From       = "itshasan573@gmail.com";
